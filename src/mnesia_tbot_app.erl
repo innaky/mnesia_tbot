e648763efda810968168d13b2451067b80e9c4b9
Innaky <innaky@protonmail.com>
2020-10-02 15:54:24 -0400

    Multiples funciones (ver abajo) get_users/0 => Retorna la lista de usuarios que han interactuado con el robot, la información contiene username, id y hora de la última ejecución de /star.
    op/2::clave, nombre de usuario => comando para agregar el username a la tabla de administradores.
    
    get_admin/1:: nombre de usuario => Retorna el nombre de usuario, su id y último /start, si no existe
    retorna una lista vacía.
    
    get_admins/0 => Retorna una lista de todos los administradores.
    
    is_hash_valid/1 :: clave => Verifica si la contraseña ingresada coíncide con el hash224, guardada en
    OPHASH.

--- src/mnesia_tbot_app.erl
+++ src/mnesia_tbot_app.erl
@@ -8,10 +8,13 @@
 -behaviour(application).
 
 -export([start/2, stop/1]).
--export([insert_user/1, install/1, get_user/1]).
+-export([insert_user/1, install/1, get_user/1, get_users/0, op/2, is_hash_valid/1, get_admin/1]).
+-export([get_admins/0]).
 
 -include_lib("stdlib/include/qlc.hrl").
 -include("include/telegram_users.hrl").
+
+-define(OPHASH, <<206,163,197,225,210,222,44,172,93,222,208,63,120,146,159,78,207,203,233,237,49,211,196,182,194,112,102,35>>).
 
 start(_StartType, _StartArgs) ->
     mnesia_tbot_sup:start_link().
@@ -48,9 +51,58 @@ get_user(Username) ->
 		qlc:e(Q)
 	end,
     mnesia:transaction(F).
+
+get_users() ->
+    F = fun() ->
+		Q = qlc:q([{U#telegram_users.username, U#telegram_users.id,
+			    U#telegram_users.localtime}
+			   || U <- mnesia:table(telegram_users)]),
+		qlc:e(Q)
+	end,
+    mnesia:transaction(F).
+
+op(Password, Username) ->
+    case is_hash_valid(Password) of
+	true ->
+	    {_, User} = get_user(Username),
+	    case User of
+		[] ->
+		    false;
+		_ ->
+		    [{Name, Id, _}] = User,
+		    Localtime = format_time(),
+		    NewOp = #telegram_admins{id = Id, username = Name, localtime = Localtime},
+		    T = fun() ->
+				mnesia:write(NewOp)
+			end,
+		    mnesia:transaction(T),
+		    ok
+	    end;
+	_ ->
+	    false
+    end.
+
+get_admin(Username) ->
+    T = fun() ->
+		Q = qlc:q([A || A <- mnesia:table(telegram_admins),
+				A#telegram_admins.username == Username]),
+		qlc:e(Q)
+	end,
+    mnesia:transaction(T).
+
+get_admins() ->
+    T = fun() ->
+		Q = qlc:q([{A#telegram_admins.id, A#telegram_admins.username,
+			    A#telegram_admins.localtime}
+			   || A <- mnesia:table(telegram_admins)]),
+		qlc:e(Q)
+	end,
+    mnesia:transaction(T).
 		
 %% internal functions
 format_time() ->
     {{Y, M, D}, {H, Min, S}} = erlang:localtime(),
     erlang:list_to_binary(io_lib:format("~p:~p:~p ~p/~p/~p", [H, Min, S, D, M, Y])).
 
+is_hash_valid(Password) ->
+    crypto:hash(sha224, Password) == ?OPHASH.
